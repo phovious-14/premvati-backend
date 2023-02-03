a7a9d6b61a859630251d0021d1699144a2839f36
phovious-14 <krishnpanchal14@gmail.com>
2023-02-03 15:02:50 +0530

    set validation

---
+++ validation.js
@@ -0,0 +1,27 @@
+const Joi = require("joi")
+
+module.exports = (data) => {
+    let schema = Joi.object({
+
+        age: Joi.string().required(),
+        gender: Joi.string().required(),
+        order: Joi.string().required(),
+        qualityfood: Joi.string().required(),
+        tasteoffood: Joi.string().required(),  
+        servingmethod: Joi.string().required(),    
+        staffbehave: Joi.string().required(),
+        cleanliness: Joi.string().required(),  
+        likedproduct: Joi.string(),    
+        suggestion: Joi.string(),    
+        contactnumber: Joi.string().min(10).max(10),    
+        formDate: Joi.date(),    
+        image: Joi.string(),    
+        cityId: Joi.number().integer().required(), 
+
+    })
+  
+    // This is a shorter version
+    const { error } = schema.validate(data);
+
+    return error;
+}
